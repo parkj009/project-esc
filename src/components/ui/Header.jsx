a4d604f65bbfad25097a0b09633e059dbef72a80
James <parkj009@gmail.com>
2022-09-29 07:32:19 -1000

    testing branch commit slack notification

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -105,7 +105,7 @@ function Header() {
                           <a
                             href="/profile"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
+                              active ? "bg-gray-200" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
                           >
