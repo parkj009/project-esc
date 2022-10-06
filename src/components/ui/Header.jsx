4e0ffbec6a0241c26682a7d7aaade80d10b15270
James Park <parkj009@gmail.com>
2022-10-05 15:58:30 -1000

    added declarative routing to mobile view links

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -134,9 +134,9 @@ function Header() {
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
                 <Disclosure.Button
+                  as={NavLink}
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  to={item.link}
                   className={classNames(
                     item.current
                       ? "bg-gray-900 text-white"
