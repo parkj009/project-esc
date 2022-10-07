9536af201ce98cc33966936ffc28e5e400c1d7a2
James Park <parkj009@gmail.com>
2022-10-06 16:14:52 -1000

    added prev start and next buttons for room details

--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -8,27 +8,39 @@ function RoomDetails() {
   const filteredRoom = room && room.rooms.filter((room) => room.id == id);
 
   return (
-    <>
-      <div className="p-4 grid grid-cols-1 gap-5">
-        <div className="px-4 py-4">
-          {filteredRoom &&
-            filteredRoom.map((room) => (
-              <div
-                key={room.id}
-                alt=""
-                className="rounded overflow-hidden shadow-lg"
-              >
-                <img className="w" src="../src/assets/react.svg" />
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{room.name}</div>
-                  <p className="text-gray-700 text-base">{room.blurb}</p>
-                  <p className="text-gray-700 text-base">{room.difficulty}</p>
+    <div className="p-4 grid grid-cols-1 w-full">
+      <div className="px-4 py-4 rounded overflow-hidden shadow-lg">
+        {filteredRoom &&
+          filteredRoom.map((room) => (
+            <div key={room.id} alt="">
+              <div className="px-6 py-4 grid grid-cols-2">
+                <div className="font-bold text-xl mb-2 text-center">
+                  <p>{room.name} Room</p>
+                  <img src="../src/assets/react.svg" />
+                </div>
+                <div className="font-bold text-xl mb-2 text-center">
+                  <p>Settings:</p>
+                </div>
+                <div className="col-span-2">
+                  <p className="font-bold text-xl mb-2">{room.blurb}</p>
+                  <p className="text-gray-700 text-base">{room.paragraph}</p>
                 </div>
               </div>
-            ))}
-        </div>
+              <div className="flex justify-between">
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
+                  Previous
+                </button>
+                <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 border-b-4 border-green-700 hover:border-green-500 rounded-lg">
+                  Start
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-lg">
+                  Next
+                </button>
+              </div>
+            </div>
+          ))}
       </div>
-    </>
+    </div>
   );
 }
 
