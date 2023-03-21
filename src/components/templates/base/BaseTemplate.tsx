a44036a9863e3b30ed82e6ea333946695a9dbfe3
James Park <parkjc@hawaii.edu>
2023-03-20 18:22:22 -1000

    add BaseTemplate component

---
+++ src/components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './BaseTemplate.module.css';
+
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
+  return (
+    <div className={styles.container}>
+      <p>{sampleTextProp}</p>
+    </div>
+  );
+};
+
+export default BaseTemplate;
